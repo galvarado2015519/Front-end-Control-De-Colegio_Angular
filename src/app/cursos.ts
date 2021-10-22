40e84edd7485669f42e55c92665c8f97f6557b8b
Gildardo Alvarado <leonelgildardogl@hotmail.com>
2021-10-21 20:11:07 -0600

    'update'

---
+++ src/app/cursos.ts
@@ -0,0 +1,30 @@
+const cursos = [{
+        id: 1,
+        nombre: 'Programación Web',
+        cupo: 20
+    },
+    {
+        id: 2,
+        nombre: 'Base de datos con Oracle',
+        cupo: 20
+    },
+    {
+        id: 3,
+        nombre: 'Patrons de diseño',
+        cupo: 20
+    },
+    {
+        id: 4,
+        nombre: 'Programación con dispositivos mòviles',
+        cupo: 20
+    },
+    {
+        id: 5,
+        nombre: 'Fundamentos de programación',
+        cupo: 20
+    }
+]
+
+export {
+    cursos
+}
